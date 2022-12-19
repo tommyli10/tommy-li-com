632d557b1a9512040fb30985e63fdff913b22d5f
tommyli97 <tommyli76278@gmail.com>
2022-12-18 22:19:18 -0800

    added about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,15 +10,15 @@ export default function about({ data }) {
         <>
             <Layout>
                 <div className='about'>
-                    <div className='intro-section'>
+                    <div className='about-intro-section'>
                         <div className='profile-pic'>
                             <GatsbyImage image={pic} alt='profile picture' />
                         </div>
                         <div className='profile-summary'>
-                            <div className='summary'>
-                                <p>Hi, I'm Tommy, a full-stack software engineer. I believe that the best engineers think like designers. Whether it is building an external API, programmatic API, or user interface, I always remind myself that keeping the user's needs in mind is the heart of a good user experience.</p>
+                            <div>
+                                <p>Hi, I'm Tommy. I like to design and build things. I am currently a freelance fullstack developer located in Los Angeles.</p>
                                 <br />
-                                <p>My experience ranges from full-stack development to system design and DevOps. I work day-to-day with JavaScript, TypeScript, React/Redux, Node/Express, Apollo GraphQL, and databases (PostgreSQL, Mongoose). I am also a big support and active contributor to open source products.</p>
+                                <p>Other than designing and creating responsive websites for individuals and small businesses, I am also an advocate and contributor for open source products that help developers.</p>
                             </div>
                             <div className='profile-links'>
                                 <Link className='button' to='mailto:tommylidev@gmail.com'>Contact me</Link>
@@ -26,8 +26,32 @@ export default function about({ data }) {
                             </div>
                         </div>
                     </div>
-                    <div className='about-details'>
-                        {/* placeholder for in depth about me  */}
+                    <div className='experiences'>
+                        <div className='profile-topic'>
+                            <h2>
+                                Process
+                            </h2>
+                        </div>
+                        <div className='profile-section-info'>
+                            <p>I believe that the best engineers think like designers. Whether it is building an external API, programmatic API, or user interface, I always remind myself that keeping the user's needs in mind is the heart of a good user experience.</p>
+                            <br />
+                            <p>While I like to learn new technologies to expand my toolset, I value working software over specific tools. This is because I've been caught in analysis paralysis and realized that I learn more by just start solving problems, and that will often lead me to iterate into better solutions.</p>
+                        </div>
+                    </div>
+                    <div className='experiences'>
+                        <div className='profile-topic'>
+                            <h2>
+                                Skills
+                            </h2>
+                        </div>
+                        <div className='profile-section-info'>
+                            <ul>
+                                <li><strong>Technical: </strong>JavaScript, React (Hooks, Router), Redux, Node.js, Express, SQL (PostgreSQL), NoSQL (MongoDB), GraphQL, Apollo, TypeScript
+                                    Authentication (bcrypt, JWT), Webpack, Git/Github, Chart.js, Chakra UI, Material UI, Bootstrap, HTML/CSS/SAS, Python, TDD (Jest, Supertest, React Testing Library),
+                                    Docker, AWS, CI/CD (AWS CodePipeline, Netlify), SEO</li>
+                                <li><strong>Graphics/3D Modeling: </strong>Photoshop, Illustrator, InDesign, Rhino, Grasshopper, SketchUp</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </Layout>
